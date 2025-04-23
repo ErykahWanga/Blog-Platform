986309c004b14fddf8a9450aa4a669122a13cf58
Your Name <githinjibrian700@gmail.com>
2025-04-23 12:21:06 +0300

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import PostList from './components/PostList.jsx';
@@ -6,31 +5,6 @@ import AddPost from './components/AddPost.jsx';
 import EditPost from './components/EditPost.jsx';
 import PostDetails from './components/PostDetails.jsx';
 import FavoritePosts from './components/FavoritePosts.jsx';
-
-function App() {
-  return (
-    <div className="bg-gray-900 min-h-screen">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<PostList />} />
-        <Route path="/add" element={<AddPost />} />
-        <Route path="/edit/:id" element={<EditPost />} />
-        <Route path="/post/:id" element={<PostDetails />} />
-        <Route path="/favorites" element={<FavoritePosts />} />
-      </Routes>
-    </div>
-  );
-}
-
-export default App;
-=======
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar.jsx'
-import PostList from './components/PostList.jsx'
-import AddPost from './components/AddPost.jsx'
-import EditPost from './components/EditPost.jsx'
-import PostDetails from './components/PostDetails.jsx'
-import FavoritePosts from './components/FavoritePosts.jsx'
 
 function App() {
   return (
@@ -46,8 +20,7 @@ function App() {
         </Routes>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
->>>>>>> 5c376fd (Refactor App component to remove unused code and streamline routing structure)
+export default App;
