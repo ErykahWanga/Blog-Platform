dbad7dcc761ff874e829f7c30a8fc8ce62767eeb
Griffin-pixel162 <ombagigriffin@gmail.com>
2025-06-24 15:40:36 +0300

    Add favorites model, controller, tests, and frontend for blog platform

---
+++ blog-backend/server/models/favorite.py
@@ -0,0 +1,13 @@
+from server.config import db
+from datetime import datetime
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', back_populates='favorites')
+    post = db.relationship('Post', back_populates='favorites')
--- blog-backend/server/models/user.py
+++ blog-backend/server/models/user.py
@@ -20,3 +20,4 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
